95870c03637e6625298c8ef5984faf666c452da9
Aidin <aidin.dadashzadeh@gmail.com>
2022-06-08 20:31:51 +0200

    config file

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import flask_cors
 from routes.mealRoutes import meal_routes
 
 flask_app = flask.Flask(__name__)
-flask_app.config['CORS_HEADERS'] = 'Content-Type'
+flask_app.config.from_pyfile('config.py')
 flask_cors.CORS(flask_app)
 
 meal_routes(flask_app)
 
+#flask_app.config.get('DB_ADDRESS')
 
 flask_app.run(debug = True)
---
+++ config.py
@@ -0,0 +1,7 @@
+DEBUG = True
+FLASK_ENV = 'development'
+CORS_HEADERS = 'Content-Type'
+
+#-----------------------------------------------------#
+DB_ADDRESS = 'insert.url.here'
+DB_KEY = 'insert.key.here'
