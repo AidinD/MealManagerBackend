4ab323b876c9acd3e80f460d256cbc72350d7bf2
Aidin <aidin.dadashzadeh@gmail.com>
2022-06-18 23:16:59 +0200

    Can accept json request body

--- routes/userRoutes.py
+++ routes/userRoutes.py
@@ -18,19 +18,23 @@ def user_routes(flask_app):
         return userController.get_user_by_name(name)
 
     @flask_app.route('/user', methods=['PUT'])
-    @cross_origin(origin='*', headers=['Content-Type', 'allow-headers'])
+    @cross_origin()
     def add_user():
         print()
-        name = request.form.get("name")
-        share = request.form.get("share")
+        content = request.json
+        name = content.get("name")
+        share = content.get("share")
         return userController.add_user(name, share)
 
     @flask_app.route('/user/<int:user_id>', methods=['PUT'])
+    @cross_origin()
     def update_user(user_id):
-        name = request.form["name"]
-        share = request.form["share"]
+        content = request.json
+        name = content.get("name")
+        share = content.get("share")
         return userController.update_user(user_id, name, share)
 
     @flask_app.route('/user/<int:user_id>', methods=['DELETE'])
+    @cross_origin()
     def delete_user(user_id):
         return userController.delete_user(user_id)
